bf4c231af2c962835e2e465091560f26dcf11159
spavlenko774 <spavlenko74@mail.ru>
2025-02-24 18:07:22 +0500

    addTest function

---
+++ app/MathTest.kt
@@ -0,0 +1,7 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+class MathTest {
+    @Test
+
+}
---
+++ app/src/test/java/com/example/gitlab/MathTest.kt
@@ -0,0 +1,13 @@
+package com.example.gitlab
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MathTest {
+    @Test
+    fun addTest()
+    {
+        assertEquals(2, 1+1)
+    }
+}
